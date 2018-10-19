b4a053c74c6a2f120c98b95aafdf12714e921b0b
Fred Khoury <fred.khoury@alloy.digital>
2018-10-19 10:32:15 -0500

    linking by route names

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
-{
+module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "node": true
+    "node": true,
   },
   "parserOptions": {
     "parser": "babel-eslint"
   },
   "extends": [
     "airbnb-base",
-    "plugin:vue/recommended"
+    "plugin:vue/recommended",
   ],
   "rules": {
     "import/no-unresolved": "off",
