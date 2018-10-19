eb60679924d74adef3abc3ce3197c4517293fc8c
Fred Khoury <fred.khoury@alloy.digital>
2018-10-19 13:06:44 -0500

    moving on back to branch cut from last lesson

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is {{ username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      require: true,
+      default: `Username`,
+    },
+  },
+};
+</script>
